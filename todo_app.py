595cb3c5ea1ce057320c70ba673db01ddbcc65d3
Bidkar Aragón Cárdenas <bidkar@cetis108.edu.mx>
2025-10-23 13:30:26 -0700

    Agregar variable de sesión para tareas y mejorar la entrada de nueva tarea en la aplicación Todo App

--- todo_app.py
+++ todo_app.py
@@ -2,6 +2,9 @@ import streamlit as st
 
 st.set_page_config(page_title="Todo App", page_icon="☑️")
 
+# variables de sesión
+tareas = ["Tarea 1", "Tarea 1", "Tarea 3"]
+
 st.title("☑️ Todo App")
 st.text("Aplicación para gestionar tareas simples")
 
@@ -9,8 +12,13 @@ with st.form("frm_agregar", clear_on_submit=True):
     col1, col2 = st.columns([6,2])
 
     with col1:
-        st.text_input("", placeholder="Nueva tarea", 
+        st.text_input("Nueva tarea", placeholder="Nueva tarea", 
                       label_visibility="collapsed")
 
     with col2:
         st.form_submit_button("Agregar", use_container_width=True)
+
+st.subheader("Tareas")
+
+for i, tarea in enumerate(tareas):
+    st.checkbox(tarea, key=f"tarea_{i}")
